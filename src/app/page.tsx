71561ea39843e8b2099f0f1862914b27156f9357
Ruslan Oyun <careerRuslanOyun@yandex.ru>
2025-07-21 00:50:13 +0600

    add wagmi and viem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { Providers } from './providers';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Crypto module</h1>
-    </div>
+    <Providers>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Crypto module</h1>
+      </div>
+    </Providers>
   );
 }
